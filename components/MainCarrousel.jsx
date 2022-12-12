1109acf6a3fd0f4de857898f12267f57c2731643
Erickrova <crachipp01@gmail.com>
2022-12-12 18:39:11 -0500

    1.0.1

--- components/MainCarrousel.jsx
+++ components/MainCarrousel.jsx
@@ -5,22 +5,22 @@ const MainCarrousel = () => {
     <div className="slider rounded-md ">
     <ul>
         <li>
-            <Image height={400} width={800} alt="carrousel image 1"  src="/carrousel/1.jpg"/>
+            <Image height={400} width={800} alt="carrousel image 1" priority src="/carrousel/1.jpg"/>
         </li>
         <li>
-            <Image height={400} width={800} alt="carrousel image 2"  src="/carrousel/2.jpg"/>
+            <Image height={400} width={800} alt="carrousel image 2" priority src="/carrousel/2.jpg"/>
         </li>
         <li>
-            <Image height={400} width={800} alt="carrousel image 3"  src="/carrousel/3.jpg"/>
+            <Image height={400} width={800} alt="carrousel image 3" priority src="/carrousel/3.jpg"/>
         </li>
         <li>
-            <Image height={400} width={800} alt="carrousel image 4"  src="/carrousel/4.jpg"/>
+            <Image height={400} width={800} alt="carrousel image 4" priority src="/carrousel/4.jpg"/>
         </li>
         <li>
-            <Image height={400} width={800} alt="carrousel image 5"  src="/carrousel/5.jpg"/>
+            <Image height={400} width={800} alt="carrousel image 5" priority src="/carrousel/5.jpg"/>
         </li>
         <li>
-            <Image quality={100} height={400} width={800} alt="carrousel image 6"  src="/carrousel/6.jpg"/>
+            <Image quality={100} height={400} width={800} alt="carrousel image 6" priority  src="/carrousel/6.jpg"/>
         </li>
     </ul>
 </div>
